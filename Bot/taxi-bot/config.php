785387b587f28789d084baf87403546606fe8161
IvanHunters <git-ivan@xyzs.ru>
2019-04-09 13:24:42 +0200

    Убрал лишнее

--- Bot/taxi-bot/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "kernel.php";
-$confirmation_token = 'af18eee1';
-$token_Group = 'd18f72ed9755148cbf4d9043d48d8707b8af5e243faf2c0fce239411cbe89281c4c309d69ba52b908b384';
-$token_User  = '22a3ee6e42d43f779282c0a94800d1d878a6f6334c10a328982b4e5094aec94c7d9578dca19a533c13bbc';
-
-
-?>
--- Bot/taxi-bot/modules.php
+++
@@ -1,8 +0,0 @@
-<?php
-$database = new database();
-$taxiR = new taxi();
-$vk = new vk($token_User, $token_Group);
-$data = json_decode(file_get_contents('php://input'));
-
-
-?>
